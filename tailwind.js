75f11dae681309399e208e492450ae29724420b2
Syed Asrar <codecraftbysyed@gmail.com>
2025-06-02 16:28:50 +0530

    Add files via upload

---
+++ tailwind.js
@@ -0,0 +1,16 @@
+// Tailwind config for dark mode
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#7e3af2",
+        accent: "#facc15",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        heading: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+};
